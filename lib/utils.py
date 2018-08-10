5c86b78b32515ff0c75a6cc96b346d2f776406cb
Nandan Rao <nandanmarkrao@gmail.com>
2018-08-10 13:22:00 +0000

    cleaning up embedding subprocess"

--- lib/utils.py
+++ lib/utils.py
@@ -4,9 +4,15 @@ import linecache
 from lib.preprocess import preprocessor
 import concurrent
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import reduce
+
 
 def get_readme(repo, attempts = 0):
-    filenames = [ 'README.md', 'readme.md', 'Readme.md', 'readme.txt', 'README.txt', 'Readme.txt']
+    """ Get readmes from Github and run through our preprocessor """
+    names = ['README', 'readme', 'Readme']
+    endings = ['', 'md', 'txt', 'markdown']
+    filenames = [n+e for n in names for e in endings]
+    print(filenames)
     try: 
         f = filenames[attempts]
     except IndexError:
@@ -21,10 +27,9 @@ def get_all_readmes(repos):
     with ThreadPoolExecutor(max_workers=100) as executor:
         return executor.map(get_readme, repos)
     
-def write_ai_readmes(repos, filename):
-    with open(filename, 'w') as f:
-        texts = get_all_readmes(repos)
-        [f.write(text+'\n') for text in texts if text]
+def get_ai_readmes(repos, filename):
+    texts = get_all_readmes(repos)
+    return reduce(lambda r,t: r + t + '\n' if t else r, texts, '')
 
 def get_embeddings(filename, size):
     embeddings = np.ones((size, 100))
@@ -42,9 +47,25 @@ def get_embeddings(filename, size):
 def get_sentence(i):
     return linecache.getline('./prepared-readmes/by_repo.txt', i+1)
 
-def get_ss_embed(filename):
-    with open(filename, 'r') as f:
-        foo = [l.strip().split(' ') for i,l in enumerate(f)]
-        vecs = [np.array([float(n) for n in s]) 
-                for i,s in enumerate(foo) if i%2 == 1]
-        return vecs
+def get_ss_embed(out):
+    arr = out.split('\n')[4:]
+    vecs = [np.array([float(n) for n in s.strip().split(' ')]) 
+            for i,s in enumerate(arr) if i%2 == 1] 
+    return vecs
+
+def get_repo_name(i):
+    """Get id from lookup... then get name from bigquery table? """
+    pass
+    
+def predict_ai(model, pos, X):
+    """ pos is the positive class vectors, X is the entire dataset """    
+    model = deepcopy(model)
+    model.fit(pos)
+    preds = model.predict(X)
+    return np.argwhere(preds == 1), model
+
+def print_random(pos, amt=30):
+    print('Printing {}/{} positive documents'.format(amt, len(pos)))
+    np.random.shuffle(pos)
+    for p in pos[0:30]: 
+        print(get_sentence(p[0]))
