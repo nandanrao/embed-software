8576fd9d2c2c280162b246447dd1b062a3e54ccf
Nandan Rao <nandanmarkrao@gmail.com>
2018-10-17 11:02:57 +0000

    organizing for modularization
    removed starspace notebook and minimalized preprocess notebook

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,5 +1,12 @@
 import re
 from sklearn.feature_extraction.text import strip_accents_ascii, strip_tags
+import pandas as pd
+from tqdm import tnrange, tqdm_notebook
+from multiprocessing import Pool
+import dataset
+import gcsfs
+import s3fs
+from os.path import join
 
 # PATTERNS
 ############################
@@ -51,6 +58,7 @@ def readme_processor(s):
     
 
 class Preprocessor():
+    # For use in notebooks, provides tqdm notebook bar
     def __init__(self, 
                  string_processor, 
                  min_words_per_sentence, 
@@ -86,3 +94,71 @@ class Preprocessor():
             return None
 
         return s
+
+
+class ParallelProcessor():    
+    jl_pattern = re.compile('.+\.jl$')
+    csv_pattern = re.compile('.+\.csv$')
+
+    def __init__(self, 
+                 string_processor, 
+                 inpath, 
+                 outpath, 
+                 content_key,                 
+                 id_key,
+                 pandas_kwargs = {},
+                 cores = None,
+                 fs = gcsfs.GCSFileSystem(project='open-source-software')):
+        self.string_processor = string_processor
+        self.inpath = inpath
+        self.outpath = outpath
+        self.id_key = id_key
+        self.content_key = content_key
+        self.pandas_kwargs = pandas_kwargs
+        self.cores = cores
+        self.fs = fs
+        
+    def _read_df(self, filename, file):
+        if re.match(self.csv_pattern, filename):
+            return pd.read_csv(file, **self.pandas_kwargs)
+        elif re.match(self.jl_pattern, filename):
+            return pd.read_json(file, lines=True, **self.pandas_kwargs)
+        else:
+             raise TypeError('Cannot parse file: {}'.format(filename))
+
+    def _read(self, filename):
+        with self.fs.open(join(self.inpath, filename)) as fi:            
+            return self._read_df(filename, fi)
+            
+    def _get_files(self):
+        files = self.fs.ls(self.inpath)
+        files = [f.split('/')[-1] for f in files]
+        return [f for f in files if f]
+    
+    def process(self,  
+                       f,  
+                       compression = 'gzip'):
+        
+        key = self.content_key
+        df = self._read(f)    
+        df = df[df[key].notna()].reset_index(drop=True)
+        
+        processed = df[key].map(self.string_processor)
+        
+        df['content'] = processed
+        df = (df[(df.content.notna()) & (df[self.id_key].notna())]
+              .reset_index(drop=True)
+              .drop(key, 1))
+    
+        return df
+        
+    def process_all(self):
+        files = self._get_files()
+        pool = Pool(self.cores)
+        conn = dataset.connect('sqlite:///{}'.format(self.outpath))
+        table = conn['processed']        
+        for df in tqdm_notebook(pool.imap(self.process, files), total=len(files)):
+            for i,c in zip(df[self.id_key], df.content):
+                table.insert_ignore({self.id_key: i, 'content': c}, [self.id_key])           
+        pool.close()
+        pool.join()
