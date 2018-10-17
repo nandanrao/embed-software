d44cc1f84064f4d582c40a8d96fef8cd09720455
Nandan Rao <nandanmarkrao@gmail.com>
2018-10-17 11:12:46 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='embed_software',
+    version='0.0.1',
+    url='https://github.com/nandanrao/embed-software',
+    py_modules=['lib'],
+    zip_safe=False,
+    install_requires=[
+        'pandas',
+        'sklearn',
+        'tqdm',
+        'dataset',
+        'diskcache'
+    ]
+)
